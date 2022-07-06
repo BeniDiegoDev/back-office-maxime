8d37c5734e3ab355b3fa6c27e2553ca551098991
BeniDiegoVlog <benjamin.donofrio@outlook.fr>
2022-07-07 00:07:21 +0200

    make it better

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,8 @@ var session = require("express-session");
 require('./models/connexion')
 
 var app = express();
+
+app.use(cors())
 
 app.use(
   session({
